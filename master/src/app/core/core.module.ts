06cc813df3c94bf27ab4928085e784929a691133
zhanfei <2024539581@qq.com>
2017-09-07 00:18:00 +0800

    no message

--- master/src/app/core/core.module.ts
+++ master/src/app/core/core.module.ts
@@ -1,11 +1,21 @@
+import {NgModule, SkipSelf, Optional} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
-import {Component} from '@angular/core';
-
-@Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
+  exports: [HeaderComponent, FooterComponent, SidebarComponent]
 })
-export class AppComponent {
-    title = 'app';
+
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error('模块已存在，不能再次加载');
+    }
+  }
 }
